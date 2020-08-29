59697c2f8adf64ecf9be8d6fde9e4e85232aed0b
Lucio Flavio C. Matias <lucioflavio.cmatias90@hotmail.com>
2020-08-29 14:21:32 -0300

    installing prettier package to fix style guide code

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
-import { Application } from 'express'
+import { Application } from 'express';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
